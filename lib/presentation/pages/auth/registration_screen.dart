7f7f4815c9132b8df908096675fdced06694c528
Leningram <yes.kanat@gmail.com>
2024-07-20 10:50:00 +0300

    datepicker added

--- lib/presentation/pages/auth/registration_screen.dart
+++ lib/presentation/pages/auth/registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -10,7 +11,25 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _formKey = GlobalKey<FormState>();
   late String _name, _email, _password, _photoUrl;
-  late int _age;
+  DateTime? _birthDate;
+  final DateFormat _dateFormat = DateFormat('yyyy-MM-dd');
+  final TextEditingController __birthDateController = TextEditingController();
+  // Метод для выбора даты
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime(2000), // Начальная дата
+      firstDate: DateTime(1900), // Самая ранняя дата
+      lastDate: DateTime.now(), // Самая поздняя дата
+    );
+    if (picked != null && picked != _birthDate) {
+      setState(() {
+        _birthDate = picked;
+        __birthDateController.text = _dateFormat.format(_birthDate!);
+      });
+    }
+  }
+
 // Метод для регистрации
   void _register() async {
     if (_formKey.currentState!.validate()) {
@@ -36,16 +55,21 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             },
             // onSaved: (value) => _name = value,
           ),
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Возраст'),
-            keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Введите возраст';
-              }
-              return null;
-            },
-            onSaved: (value) => _age = int.parse(value!),
+          GestureDetector(
+            onTap: () => _selectDate(context),
+            child: AbsorbPointer(
+              child: TextFormField(
+                controller: __birthDateController,
+                decoration: InputDecoration(
+                  labelText: 'Дата рождения',
+                  hintText: _birthDate == null
+                      ? 'Выберите дату'
+                      : _dateFormat.format(_birthDate!),
+                ),
+                validator: (value) =>
+                    _birthDate == null ? 'Выберите дату рождения' : null,
+              ),
+            ),
           ),
           TextFormField(
             decoration: const InputDecoration(labelText: 'Email'),
